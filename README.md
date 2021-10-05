fb6e94ca6d4a1fa2614930437b290df7c6827aa4
erwanjb <wawan.jb@gmail.com>
2021-10-05 18:28:19 +0200

    init project

---
+++ README.md
@@ -0,0 +1,42 @@
+## Description
+
+Test pour Hubvisory Source
+But : Réaliser un Quizz cinématographique en accord avec les spécifications techniques.
+Voir le board du Quizz [ICI](https://github.com/erwanjb/test-hubvisory-source/projects/1)
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Building the app
+
+```bash
+$ npm run build
+```
+
+## Running the app
+
+```bash
+$ npm run start
+```
+
+## Building and Running the app
+
+```bash
+$ npm run full
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+ 
+module.exports = {
+  entry: "./src/client/index.tsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".jsx", ".js", ".json"]
+  },
+  output: {
+    path: path.join(__dirname, "dist/client"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            plugins: ["@babel/plugin-transform-regenerator", "@babel/plugin-transform-runtime"],
+            presets: [
+              [
+                "@babel/preset-env",
+                { targets: { browsers: "last 2 versions" } } // or whatever your project requires
+              ],
+              "@babel/preset-typescript",
+              "@babel/preset-react"
+            ],
+          }
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'dist-react/fonts/',
+              publicPath: '/dist-react/fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'dist-react/images/',
+          publicPath: '/dist-react/images/'
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  }
+};
